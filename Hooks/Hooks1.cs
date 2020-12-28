592dd4e3259f55dbbc55b0a4f3fc9471c32c45c0
Jerren Every <jerren1122@hotmail.com>
2020-12-28 05:10:32 -0500

    We are going to commit this

---
+++ Hooks/Hooks1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace CSpecFlowSelenium.Hooks
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        ChromeDriver driver;
+
+         [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver("C:/Users/Jerren/source/repos/CSpecFlowSelenium/Drivers");
+            driver.Navigate().GoToUrl(@"http://w3schools.com");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+        //do studff
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,16 @@
+﻿Good Overview on Implementing SpecFlow: https://docs.specflow.org/projects/getting-started/en/latest/GettingStarted
+How to Implement Selenium and Chrome: https://www.c-sharpcorner.com/article/selenium-automation-test-cases-for-the-net-web-application/
+ 
+We need to utilize Validator
+
+We need to utilize Page Object
+
+We need to utilize Hooks
+-Need to know how to utilize conditional hooks as well
+
+We need to run from the command line and create a batch file that captures those steps 
+
+We need to know how tagging works. 
+-Be able to exclude and include tests 
+
+Ensure that it works on another machine 
